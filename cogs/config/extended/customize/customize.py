78c10d26962e41df8af87f5c27d08f48d8204d0c
nxyy <175990828+nxyystore@users.noreply.github.com>
2025-11-27 14:24:45 -0500

    g

--- cogs/config/extended/customize/customize.py
+++ cogs/config/extended/customize/customize.py
@@ -1,6 +1,6 @@
 import discord
 
-from discord.ext.commands import group, has_permissions
+from discord.ext.commands import hybrid, hybrid_group, has_permissions
 
 from core.client.context import Context
 from tools import CompositeMetaClass, MixinMeta
@@ -11,7 +11,7 @@ class Customize(MixinMeta, metaclass=CompositeMetaClass):
     Cog for customizing bot appearance.
     """
     
-    @group(name="customize", aliases=["customization", "customise", "custom"], invoke_without_command=True)
+    @hybrid_group(name="customize", aliases=["customization", "customise", "custom"], invoke_without_command=True)
     @has_permissions(manage_guild=True)
     async def customize(self, ctx: Context):
         return await ctx.send_help(ctx.command)
@@ -23,17 +23,21 @@ class Customize(MixinMeta, metaclass=CompositeMetaClass):
         
     @customize.command(name="pfp", aliases=["avatar"])
     @has_permissions(manage_guild=True)
-    async def customize_pfp(self, ctx: Context, url: str):
+    async def customize_pfp(self, ctx: Context, url: str=None):
         if url is None:
             if ctx.message.attachments:
-                url = ctx.message.attachments[0].url
+                attachment = ctx.message.attachments[0]
+                fp = await attachment.to_file()
             elif ctx.message.reference:
                 ref_msg: Message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
                 if ref_msg.attachments:
-                    url = ref_msg.attachments[0].url
+                    attachment = ref_msg.attachments[0]
+                    fp = await attachment.to_file()
+                else:
+                    return await ctx.warn("Please provide a URL or attachment for the profile picture.")
             else:
                 return await ctx.warn("Please provide a URL or attachment for the profile picture.")
-        await EditMe(self.bot).edit_pfp(ctx, url)
+        await EditMe(self.bot).edit_pfp(ctx, fp)
         
     @customize.command(name="banner")
     @has_permissions(manage_guild=True)
@@ -46,7 +50,7 @@ class Customize(MixinMeta, metaclass=CompositeMetaClass):
                 if ref_msg.attachments:
                     url = ref_msg.attachments[0].url
             else:
-                return await ctx.warn("Please provide a URL or attachment for the profile picture.")
+                return await ctx.warn("Please provide a URL or attachment for the profile banner.")
         await EditMe(self.bot).edit_banner(ctx, url)
         
     @customize.command(name="reset")
