f7f6cdd05f70d8b3c2e8b33ce8183e1cd621c8a1
nxyy <175990828+nxyystore@users.noreply.github.com>
2025-12-05 20:05:33 -0500

    yeah

--- cogs/network/network.py
+++ cogs/network/network.py
@@ -1,8 +1,8 @@
+import uvicorn
+import asyncio
 from .routes import router
 
-
 from main import Evict
-
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -18,8 +18,9 @@ class Network(Cog):
         self.app = FastAPI(
             title="warm.lat API", 
             version="2.0.1", 
-            debug=True
+            debug=False
         )
+        self.app.state.bot = bot
         
         self.app.add_middleware(
             CORSMiddleware,
@@ -30,3 +31,17 @@ class Network(Cog):
         )
 
         self.app.include_router(router)
+
+        config = uvicorn.Config(
+            self.app,
+            host="0.0.0.0",
+            port=8000,
+            log_level="info",
+        )
+        self.server = uvicorn.Server(config)
+        self.task = asyncio.create_task(self.server.serve())
+
+    def cog_unload(self):
+        """Handle cleanup when the cog is unloaded."""
+        self.server.should_exit = True
+        self.task.cancel()
