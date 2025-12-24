c1beaf039ccb20e58501a2cee9e61e57bb258946
nxyy <175990828+nxyystore@users.noreply.github.com>
2025-12-24 15:29:19 -0500

    fix lavalink server

--- config.py
+++ config.py
@@ -172,7 +172,7 @@ class CLIENT:
 
 class LAVALINK:
     NODE_COUNT: int = 2
-    HOST: str = "public.rive.wtf"
+    HOST: str = "lavalink.rive.wtf"
     PORT: int = 443
     PASSWORD: str = "youshallnotpass"
     SPOTIFY_CLIENT_ID: str = "4c3c2ba7ad6c4ef4a0c0d793cb979ce8"
