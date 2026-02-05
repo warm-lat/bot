f2ac513a5a61d6a066f83f7b12001297ac36b5d2
nxyy <175990828+nxyystore@users.noreply.github.com>
2026-02-04 23:26:33 -0500

    Update config.py

--- config.py
+++ config.py
@@ -174,7 +174,7 @@ class LAVALINK:
     NODE_COUNT: int = 2
     HOST: str = "expel.best"
     PORT: int = 443
-    PASSWORD: str = "idontknowifyoushouldpass"
+    PASSWORD: str = "publicnodeaccessforallthatneednotwant"
     SPOTIFY_CLIENT_ID: str = "4c3c2ba7ad6c4ef4a0c0d793cb979ce8"
     SPOTIFY_CLIENT_SECRET: str = "12dd934e7bca4536bb9f8d86b049a25e"
     
