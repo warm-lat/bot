2b8305520c416d8bdd0d73ae1448d7874d3613bf
nxyy <175990828+nxyystore@users.noreply.github.com>
2026-05-06 10:30:43 -0400

    Update config.py

--- config.py
+++ config.py
@@ -161,7 +161,7 @@ class LAVALINK:
     NODE_COUNT: int = 2
     HOST: str = "swappi.ng"
     PORT: int = 443
-    PASSWORD: str = "st3dN0de4Y"
+    PASSWORD: str = "UnL!st3dN0de4Y@"
     SPOTIFY_CLIENT_ID: str = "4c3c2ba7ad6c4ef4a0c0d793cb979ce8"
     SPOTIFY_CLIENT_SECRET: str = "12dd934e7bca4536bb9f8d86b049a25e"
 
